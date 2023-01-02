9f047e27531f442ea8a6cfaa80ecba176c603b19
lorenammp <lorenammpaula@gmail.com>
2023-01-02 04:58:15 -0300

    feat: adicionada documentação

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -17,6 +17,8 @@ from datetime import timedelta
 import os
 import dotenv
 
+import dj_database_url
+
 dotenv.load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
