e1d15d0e48698f6b50fe1e217df867d446bd2adb
lorenammp <lorenammpaula@gmail.com>
2023-01-02 04:53:20 -0300

    feat: adicionada documentação

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["web-production-7fde.up.railway.app", "0.0.0.0"]
+
 
 
 # Application definition
